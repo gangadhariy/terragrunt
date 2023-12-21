6ed35674d540d11d68ed679b77f9b9d84c6ac41f
gangadhariy <121916303+gangadhariy@users.noreply.github.com>
2023-12-21 20:34:51 +0530

    Update output.tf

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -1,18 +1,18 @@
 output "resource_group_name" {
-  value = azurerm_kubernetes_cluster.dev.resource_group_name
+  value = azurerm_kubernetes_cluster.aks.resource_group_name
 }
 
 output "cluster_name" {
-  value = azurerm_kubernetes_cluster.dev.name
+  value = azurerm_kubernetes_cluster.aks.name
 }
 
 output "location" {
-  value = azurerm_kubernetes_cluster.dev.location
+  value = azurerm_kubernetes_cluster.aks.location
 }
 
 
 output "environment" {
-  value = azurerm_kubernetes_cluster.dev.tags.environment
+  value = azurerm_kubernetes_cluster.aks.tags.environment
 }
 
 
