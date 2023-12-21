79f434795181a0279159d51a08fe5b4fc5da9a16
gangadhariy <121916303+gangadhariy@users.noreply.github.com>
2023-12-21 13:49:38 +0530

    Update main.tf

--- modules/prod-aks/main.tf
+++ modules/prod-aks/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "aks" {
    name  =  var.resource_group_name
    location = var.location
 }
-resource "azurerm_kubernetes_cluster" "default" {
+resource "azurerm_kubernetes_cluster" "prod" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = azurerm_resource_group.aks.name
@@ -10,7 +10,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   kubernetes_version  = "1.28.3"
 
   default_node_pool {
-    name            = "default"
+    name            = "prod"
     node_count      = 1
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
@@ -28,5 +28,5 @@ resource "azurerm_kubernetes_cluster" "default" {
 
 resource "local_file" "kubeconfig" {
   content = azurerm_kubernetes_cluster.default.kube_config_raw
-  filename = "kubeconfig"
+  filename = "kubeconfig-prod"
 }
