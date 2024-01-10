536ceb5e93c2259485ecca453bd243261dcb8ef3
gangadhariy <121916303+gangadhariy@users.noreply.github.com>
2024-01-10 10:41:04 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     node_count      = 1
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
-    subnet_id       = azurerm_subnet.subnet.id
+    vnet_subnet_id  = azurerm_subnet.subnet.id
   }
 
   identity {
